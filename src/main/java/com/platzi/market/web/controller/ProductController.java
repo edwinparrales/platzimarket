fe499b10ae7c7e26df51ee6753b880fcc1d5228c
edwinparrales <edwinparrales82@gmail.com>
2021-09-06 00:07:35 -0500

    Desarrollando el controlador

---
+++ src/main/java/com/platzi/market/web/controller/ProductController.java
@@ -0,0 +1,39 @@
+package com.platzi.market.web.controller;
+
+import com.platzi.market.dominio.Product;
+import com.platzi.market.dominio.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/producto")
+public class ProductController {
+    @Autowired
+    private ProductService productService;
+    //Retornar la lista de productos http://localhost:9090/producto/all
+    @GetMapping("/all")
+    public List<Product> getAll(){
+        return productService.getAll();
+    }
+    @GetMapping("/{id}")
+    public Optional<Product> getProducto(int productId){
+       return productService.getProduct(productId);
+    }
+
+
+    public Optional<List<Product>> getByCategory(int categoryId){
+        return productService.getByCategory(categoryId);
+    }
+    public Product save(Product product){
+        return productService.save(product);
+    }
+
+    public boolean delete(int productId){
+      return productService.delete(productId);
+    }
+}
